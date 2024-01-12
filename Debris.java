fa815e6d78cff67b85359d31e70441527de374e3
ryanchiu727 <151646504+ryanchiu727@users.noreply.github.com>
2024-01-12 16:15:54 -0500

    Update Debris.java

--- Debris.java
+++ Debris.java
@@ -12,9 +12,11 @@ public class Debris extends SpaceObjects
      * Act - do whatever the Debris wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    Rocks rocks = new Rocks();
     public Debris()
     {
         getImage().scale(getImage().getWidth()/2, getImage().getHeight()/2);
+        setRotation(rocks.getRotation());
         setRotation(Greenfoot.getRandomNumber(360));
     }
     public void act()
